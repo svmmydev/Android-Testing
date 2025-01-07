3a6ac2a29901c52ba0b127ffb1aa19a23e792a28
Sammy <svmmydev@gmail.com>
2025-01-07 18:21:00 +0100

    First commit

---
+++ app/src/main/java/es/lasalle/pr2/MainActivity.kt
@@ -0,0 +1,56 @@
+package es.lasalle.pr2
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import es.lasalle.pr2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setupBinding()
+        configureUi()
+        setupUi()
+    }
+
+    private fun setupUi() = with(binding) {
+        registerButton.setOnClickListener {
+            if (emailFieldEditText.text.toString().isNotEmpty()) {
+                startActivity(Intent(applicationContext, ResultActivity::class.java).apply {
+                    putExtra("email", emailFieldEditText.text.toString())
+                })
+                finish()
+            }
+        }
+        needHelpButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse(getString(R.string.url)))
+            startActivity(intent)
+        }
+    }
+
+    private fun configureUi() {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun setupBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/es/lasalle/pr2/ResultActivity.kt
@@ -0,0 +1,41 @@
+package es.lasalle.pr2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import es.lasalle.pr2.databinding.ActivityResultBinding
+
+class ResultActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResultBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupBinding()
+        configureUi()
+        setupUi()
+    }
+
+    private fun setupUi() = with(binding) {
+        buttonFinish.setOnClickListener {
+            finish()
+        }
+        textViewEmail.text = "example@test.com"
+    }
+
+    private fun configureUi() {
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.result)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun setupBinding() {
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
